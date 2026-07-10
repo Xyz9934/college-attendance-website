5eb74702234d05a58e97bceaa64763d1b126eed2
Faizan Iqubal <faizaniqubal2008@gmail.com>
2026-07-10 15:17:05 +0530

    Allow authorization header in CORS

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ function setCorsHeaders(res, reqOrigin = "") {
     res.setHeader("Vary", "Origin");
     res.setHeader("Access-Control-Allow-Credentials", "true");
   }
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
 }
 
